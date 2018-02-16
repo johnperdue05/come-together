4b14b2b348b25b5d468f9d9516c900557c41a4d9
John Perdue <john_perdue05@yahoo.com>
2018-02-15 23:10:11 -0600

    Initial commit

---
+++ src/main/java/com/johnperdue/cometogether/ComeTogetherApplication.java
@@ -0,0 +1,12 @@
+package com.johnperdue.cometogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeTogetherApplication.class, args);
+	}
+}
